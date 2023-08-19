b4e6c1d943032a6272368444ffe3835380d84fcd
Arya <arya.tesla1856@gmail.com>
2023-08-19 09:53:03 -0700

    added main section and changed scrolling options

--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,18 @@
 function go_skills(){
-  document.body.scrollTop = 680;
-  document.documentElement.scrollTop = 680;
+  document.body.scrollTop = 790;
+  document.documentElement.scrollTop = 790;
 }
 function go_projects(){
-  document.body.scrollTop = 1550;
-  document.documentElement.scrollTop = 1550;
+  document.body.scrollTop = 1660;
+  document.documentElement.scrollTop = 1660;
 }
 function go_resume(){
-  document.body.scrollTop = 2380;
-  document.documentElement.scrollTop = 2380;
+  document.body.scrollTop = 2490;
+  document.documentElement.scrollTop = 2490;
 }
 function go_contact(){
-  document.body.scrollTop = 3190;
-  document.documentElement.scrollTop = 3190;
+  document.body.scrollTop = 3300;
+  document.documentElement.scrollTop = 3300;
 }
 function reload_page(){
   location.reload();
