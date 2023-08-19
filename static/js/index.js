02298d6b411ca48e3479cf439d03b189421c6ed6
Arya <arya.tesla1856@gmail.com>
2023-08-19 06:46:28 -0700

    first commit

---
+++ static/js/index.js
@@ -0,0 +1,20 @@
+function go_skills(){
+  document.body.scrollTop = 680;
+  document.documentElement.scrollTop = 680;
+}
+function go_projects(){
+  document.body.scrollTop = 1550;
+  document.documentElement.scrollTop = 1550;
+}
+function go_resume(){
+  document.body.scrollTop = 2380;
+  document.documentElement.scrollTop = 2380;
+}
+function go_contact(){
+  document.body.scrollTop = 3190;
+  document.documentElement.scrollTop = 3190;
+}
+function reload_page(){
+  location.reload();
+}
+AOS.init();
