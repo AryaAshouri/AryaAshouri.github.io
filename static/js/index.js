cd463f22eda017f74bd0f500edd35090c8a287d5
Arya <arya.tesla1856@gmail.com>
2023-08-24 01:58:11 -0700

    added posters and fixed animations for other devices

--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,34 @@
 function go_skills(){
-  document.body.scrollTop = 790;
-  document.documentElement.scrollTop = 790;
+  if (window.innerWidth <= 900){
+    document.body.scrollTop = 610;
+  }
+  else {
+    document.body.scrollTop = 790;
+  }
 }
 function go_projects(){
-  document.body.scrollTop = 1660;
-  document.documentElement.scrollTop = 1660;
+  if (window.innerWidth <= 900){
+    document.body.scrollTop = 2790;
+  }
+  else {
+    document.body.scrollTop = 1660;
+  }
 }
 function go_resume(){
-  document.body.scrollTop = 2490;
-  document.documentElement.scrollTop = 2490;
+  if (window.innerWidth <= 900){
+    document.body.scrollTop = 4160;
+  }
+  else {
+    document.body.scrollTop = 2490;
+  }
 }
 function go_contact(){
-  document.body.scrollTop = 3300;
-  document.documentElement.scrollTop = 3300;
+  if (window.innerWidth <= 900){
+    document.body.scrollTop = 5030;
+  }
+  else {
+    document.body.scrollTop = 3300;
+  }
 }
 function reload_page(){
   location.reload();
