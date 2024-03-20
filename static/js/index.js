0c77656fef8aa2f3fe1cfa8da15c6c886da5b114
Arya <arya.tesla1856@gmail.com>
2024-03-20 01:10:21 -0700

    added brightness-mode

--- static/js/index.js
+++ static/js/index.js
@@ -32,11 +32,44 @@ function go_summary(){
 }
 function go_contact(){
   if (window.innerWidth <= 900){
-    document.body.scrollTop = 5960;
+    document.body.scrollTop = 5940;
   }
   else {
     document.body.scrollTop = 3940;
   }
+}
+function change_brightness_mode(){
+  if (document.getElementById("brightness-icon").getAttribute("src") == "static/img/icon/light/light.png"){
+    document.documentElement.style.setProperty("--main-theme-color", "rgb(0, 174, 255)");
+    document.documentElement.style.setProperty("--another-theme-color", "rgb(51, 190, 255)");
+    document.documentElement.style.setProperty("--another-deep-color", "rgb(71, 197, 255)");
+    document.getElementById("web-icon").setAttribute("src", "static/img/icon/light/web.png");
+    document.getElementById("pc-icon").setAttribute("src", "static/img/icon/light/pc.png");
+    document.getElementById("ic-icon").setAttribute("src", "static/img/icon/light/ic.png");
+    document.getElementById("db-icon").setAttribute("src", "static/img/icon/light/db.png");
+    document.getElementById("id-icon-1").setAttribute("src", "static/img/icon/light/id.png");
+    document.getElementById("id-icon-2").setAttribute("src", "static/img/icon/light/id.png");
+    document.getElementById("id-icon-3").setAttribute("src", "static/img/icon/light/id.png");
+    document.getElementById("id-icon-4").setAttribute("src", "static/img/icon/light/id.png");
+    document.getElementById("brightness-icon").setAttribute("src", "static/img/icon/dark/dark.png")
+  }
+  else {
+    document.documentElement.style.setProperty("--main-theme-color", "rgb(30, 27, 51)");
+    document.documentElement.style.setProperty("--another-theme-color", "rgb(35, 31, 56)");
+    document.documentElement.style.setProperty("--another-deep-color", "rgb(38, 34, 61)");
+    document.getElementById("web-icon").setAttribute("src", "static/img/icon/dark/web.png");
+    document.getElementById("pc-icon").setAttribute("src", "static/img/icon/dark/pc.png");
+    document.getElementById("ic-icon").setAttribute("src", "static/img/icon/dark/ic.png");
+    document.getElementById("db-icon").setAttribute("src", "static/img/icon/dark/db.png");
+    document.getElementById("id-icon-1").setAttribute("src", "static/img/icon/dark/id.png");
+    document.getElementById("id-icon-2").setAttribute("src", "static/img/icon/dark/id.png");
+    document.getElementById("id-icon-3").setAttribute("src", "static/img/icon/dark/id.png");
+    document.getElementById("id-icon-4").setAttribute("src", "static/img/icon/dark/id.png");
+    document.getElementById("brightness-icon").setAttribute("src", "static/img/icon/light/light.png")
+  }
+}
+function change_language(){
+  
 }
 function reload_page(){
   location.reload();
